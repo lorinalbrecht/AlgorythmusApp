7cad63ca868223cd8d80396459a45d1d6e93ef0e
Lorin Albrecht <lorin.albrecht@buhlergroup.com>
2023-05-12 09:20:38 +0200

    Initialized BaseController

--- AlgorythmusApp/Controllers/BaseController.cs
+++ AlgorythmusApp/Controllers/BaseController.cs
@@ -8,5 +8,40 @@ namespace AlgorythmusApp.Controllers
         {
             return View();
         }
+
+        public void PlaceBuilding()
+        {
+            // Position einlesen
+
+        }
+
+        public void UpgradeBuilding()
+        {
+
+        }
+
+        public int CalcTiberiumCount()
+        {
+            int test = 0;
+            return test;
+        }
+
+        public int CalcPowerCount()
+        {
+            int test = 0;
+            return test;
+        }
+
+        public int CalcBuildingCount()
+        {
+            int test = 0;
+            return test;
+        }
+
+        public int CalcBuildingMax()
+        {
+            int test = 0;
+            return test;
+        }
     }
 }
