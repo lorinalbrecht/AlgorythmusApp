363f8c9d4941849d1b3f772729069c5681490a68
Suvajac Filip, LA, BUZ <filip.suvajac@buhlergroup.com>
2023-05-12 13:59:32 +0200

    silo object

--- AlgorythmusApp/Controllers/BuildingController.cs
+++ AlgorythmusApp/Controllers/BuildingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlgorythmusApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlgorythmusApp.Controllers
 {
@@ -9,13 +10,15 @@ namespace AlgorythmusApp.Controllers
             return View();
         }
 
-        public string[,] Field = { { "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9" },
-                                   { "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9" },
-                                   { "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9" },
-                                   { "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9" },
-                                   { "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9" },
-                                   { "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9" },
-                                   { "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9" },
-                                   { "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9" } };
+        public int[,] Field =    { { 11, 12, 13, 14, 15, 16, 17, 18, 19 },
+                                   { 21, 22, 23, 24, 25, 26, 27, 28, 29 },
+                                   { 31, 32, 33, 34, 35, 36, 37, 38, 39 },
+                                   { 41, 42, 43, 44, 45, 46, 47, 48, 49 },
+                                   { 51, 52, 53, 54, 55, 56, 57, 58, 59 },
+                                   { 61, 62, 63, 64, 65, 66, 67, 68, 69 },
+                                   { 71, 72, 73, 74, 75, 76, 77, 78, 79 },
+                                   { 81, 82, 83,84, 85, 86, 87, 88, 89 } };
+
+        BuildingInBase Silo1 = new BuildingInBase( 1, 3, 4, 8, 2, 5);
     }
 }
