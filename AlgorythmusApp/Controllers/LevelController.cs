0fba0c972bf0b86fe086c1867072881b2c7dfa61
Lorin Albrecht <lorin.albrecht@buhlergroup.com>
2023-05-12 11:14:02 +0200

    Optimized Seed Function for Levels

--- AlgorythmusApp/Controllers/LevelController.cs
+++ AlgorythmusApp/Controllers/LevelController.cs
@@ -15,16 +15,16 @@ namespace AlgorythmusApp.Controllers
         // Person person1 = new Person("Leopold", 6);
         public void Seed()
         {
-            Level lvl1 = new Level(1, 1);
-            Level lvl2 = new Level(2, 2);
-            Level lvl3 = new Level(3, 3);
-            Level lvl4 = new Level(4, 4);
-            Level lvl5 = new Level(5, 5);
-            Level lvl6 = new Level(6, 6);
-            Level lvl7 = new Level(7, 7);
-            Level lvl8 = new Level(8, 8);
-            Level lvl9 = new Level(9, 9);
-            Level lvl10 = new Level(10, 10);
+            Level lvl1 = new(1, 1);
+            Level lvl2 = new(2, 2);
+            Level lvl3 = new(3, 3);
+            Level lvl4 = new(4, 4);
+            Level lvl5 = new(5, 5);
+            Level lvl6 = new(6, 6);
+            Level lvl7 = new(7, 7);
+            Level lvl8 = new(8, 8);
+            Level lvl9 = new(9, 9);
+            Level lvl10 = new(10, 10);
         }
     }
 }
