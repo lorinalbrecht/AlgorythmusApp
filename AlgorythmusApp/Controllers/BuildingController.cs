c8420d9285fca2cc2e18287cb0e7baee6b58f092
Lorin Albrecht <lorin.albrecht@buhlergroup.com>
2023-05-12 14:00:34 +0200

    Shorter Form of Silo Object BuildingInBase

--- AlgorythmusApp/Controllers/BuildingController.cs
+++ AlgorythmusApp/Controllers/BuildingController.cs
@@ -19,6 +19,6 @@ namespace AlgorythmusApp.Controllers
                                    { 71, 72, 73, 74, 75, 76, 77, 78, 79 },
                                    { 81, 82, 83,84, 85, 86, 87, 88, 89 } };
 
-        BuildingInBase Silo1 = new BuildingInBase( 1, 3, 4, 8, 2, 5);
+        BuildingInBase Silo1 = new( 1, 3, 4, 8, 2, 5);
     }
 }
